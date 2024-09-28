34722f3df4490d6d342044c9ddf2d49c997d9e73
Furkan Emin Kalay <f.kalay@ditpsoftware.nl>
2024-09-28 12:52:51 +0200

    data response succesful

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
 ETHERSCAN_API_KEY = os.getenv('ETHERSCAN_API_KEY')
-WALLET_ADDRESS = '0xC8F3A121d068d85d91c5CEE5e714ce5892BCb7BC'
+wallet_address = '0xC8F3A121d068d85d91c5CEE5e714ce5892BCb7BC'
+PRICE_KAPSALON = 14
+
+print('Welcome to KapsalonsForGas. \nHere you can calculate how many kapsalons you couldve bought with your wasted gas fees')
+# wallet_address = input("What is your Ethereum wallet address?")
+
+def get_transactions(etherscan_api_key, wallet_address):
+    url = 'https://api.etherscan.io/api?module=account&action=txlist&address=' + wallet_address + '&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=' + etherscan_api_key
+    response = requests.get(url)
+    data = response.json()
+
+    if data['status'] == '1':
+        print('Succes')
+        return data['result']
+    else:
+        print('Error')
+
+
+
+get_transactions(ETHERSCAN_API_KEY,wallet_address)
 
 
