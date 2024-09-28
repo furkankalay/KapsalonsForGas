aeef255c9d89ef390cb04b723971ea17828574fd
Furkan Emin Kalay <f.kalay@ditpsoftware.nl>
2024-09-28 12:08:04 +0200

    created .env file for security reasons and regenerated api key

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-wallet_addres = '0xC8F3A121d068d85d91c5CEE5e714ce5892BCb7BC'
-API_KEY = 'HPSWR24AR3SW21P8JSIBF4MQQDTWVU77VX'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ETHERSCAN_API_KEY = os.getenv('ETHERSCAN_API_KEY')
+WALLET_ADDRESS = '0xC8F3A121d068d85d91c5CEE5e714ce5892BCb7BC'
+
+
