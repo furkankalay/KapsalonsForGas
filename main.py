c81c78d7fb0b865d811171e217507dd9b680535d
Furkan <furkaneminkalay@gmail.com>
2024-09-29 11:53:43 +0200

    api for eth prices

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import requests
 load_dotenv()
 
 ETHERSCAN_API_KEY = os.getenv('ETHERSCAN_API_KEY')
+COINGECKO_API_KEY = os.getenv('COINGECKO_API_KEY')
 wallet_address = '0x6a4a3b12E1ccD0DF693014b78702e866Cd3b0EE1'
 PRICE_KAPSALON = 14
+
 
 print('Welcome to KapsalonsForGas. \nHere you can calculate how many kapsalons you couldve bought with your wasted gas fees')
 # wallet_address = input("What is your Ethereum wallet address?")
@@ -22,6 +24,19 @@ def get_transactions(etherscan_api_key, wallet_address):
     else:
         print('Error')
 
+def get_eth_price(coingecko_api_key, wallet_address):
+    url = "https://pro-api.coingecko.com/api/v3/coins/id"
+
+    headers = {
+        "accept": "application/json",
+        "x-cg-pro-api-key": coingecko_api_key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.text)
+
+
 
 def get_total_fees_in_eth():
     transactions = get_transactions(ETHERSCAN_API_KEY,wallet_address)
@@ -31,12 +46,15 @@ def get_total_fees_in_eth():
        gas_in_wei = int(transaction['gasUsed']) * int(transaction['gasPrice'])
        total_gas_in_wei = gas_in_wei + total_gas_in_wei
 
-    print(len(transactions))
-
     total_gas_in_eth = total_gas_in_wei / (10**18)
     return total_gas_in_eth
 
-print(get_total_fees_in_eth())
+
+
+
+
+wasted_eth = float(get_total_fees_in_eth())
+print('\nYou wasted:\n' + str(round(wasted_eth,2)) +  ' ETH \n' )
 
 
 
